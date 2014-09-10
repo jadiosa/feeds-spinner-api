00000ec6c0534a991cf9461d248d753fa80c9861
jadiosa <jadiosa@gmail.com>
2014-09-09 23:48:59 -0500

    ajustes en la conexion con mongolab

--- routes/feed.js
+++ routes/feed.js
@@ -3,35 +3,21 @@
  * GET feeds listing.
  */
 
-var mongo = require('mongodb');
- 
-var Server = mongo.Server,
-    Db = mongo.Db,
-    BSON = mongo.BSONPure;
-
+var MongoClient = require('mongodb').MongoClient
   
 //Se crea con el fin de encontrar una base de datos apropiada para conectar.
-var uri = process.env.MONGOLAB_URI || 'localhost';
-console.log("Uri: "+uri) ; 
+var uri = process.env.MONGOLAB_URI || 'mongodb://localhost:27017/pescadorescolombiadb';
 
-var server = new Server(uri, 27017, {auto_reconnect: true});
-db = new Db('pescadorescolombiadb', server);
- 
-db.open(function(err, db) {
-    if(!err) {
-        console.log("Connected to 'pescadorescolombiadb' database");
-        db.collection('feeds', {strict:true}, function(err, collection) {
-            if (err) {
-                console.log("The 'feeds' collection doesn't exist");
-            }
-        });
-    }
-});
 
 exports.findAll = function(req, res) {
-    db.collection('feeds', function(err, collection) {
-        collection.find().toArray(function(err, items) {
-            res.send(items);
-        });
+    MongoClient.connect(uri, function(err, db) {
+	    if(err) throw err;
+
+	    db.collection('feeds', function(err, collection) {
+	        collection.find().toArray(function(err, items) {
+	            res.send(items);
+	            db.close();
+	        });
+	    });
     });
 };
