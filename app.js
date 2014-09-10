69bd0351b4fdeb5d62996ce081f934563d284c55
jadiosa <jadiosa@gmail.com>
2014-09-09 23:57:42 -0500

    se agregan header para Access-Control-Allow-Origin

--- app.js
+++ app.js
@@ -27,6 +27,13 @@ if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
+//add
+app.all('*', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
+ });
+
 app.get('/', routes.index);
 app.get('/feed', feed.findAll);
 
