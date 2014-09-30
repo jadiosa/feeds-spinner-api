69b17995bd47f8fa814586e6eb729593c0ed29b8
jadiosa <jadiosa@gmail.com>
2014-09-29 20:20:00 -0500

    ajstes en CORS

--- app.js
+++ app.js
@@ -27,12 +27,16 @@ if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
-//add
+//CORS on ExpressJS
 app.all('*', function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,HEAD,DELETE');
-  res.header("Access-Control-Allow-Headers", "content-Type,x-requested-with");
-  next();
+	res.header("Access-Control-Allow-Origin", "*");
+  	res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  	next();
+
+  //res.header("Access-Control-Allow-Origin", "*");
+  //res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,HEAD,DELETE');
+  //res.header("Access-Control-Allow-Headers", "content-Type,x-requested-with");
+  //next();
  });
 
 app.get('/', routes.index);
