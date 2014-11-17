4563438c6cf785169697e8670e51e3f6e72d2dfc
jadiosa <jadiosa@gmail.com>
2014-11-17 18:07:03 -0500

    se mueve feed.js de routes para lib

--- app.js
+++ app.js
@@ -5,18 +5,18 @@
 
 var express = require('express');
 var routes = require('./routes');
-var feed = require('./routes/feed');
+var feed = require('./lib/feed');
 var http = require('http');
 var path = require('path');
 
-var app = express();
+var app = exports.app = express();
 
 // all environments
 app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 app.use(express.favicon());
-app.use(express.logger('dev'));
+//app.use(express.logger('dev'));
 app.use(express.bodyParser());
 app.use(express.methodOverride());
 app.use(app.router);
